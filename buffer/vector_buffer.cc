04b86569f7172e44929602c7ba6c5ad1320d1bc9
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2025-11-24 00:15:30 -0800

    simple buffer impl
    * brute force impl is just a vector of strings.
    * buffer api won't specify i/o operations. it is meant to be an
      in-memory representation of the file.

---
+++ buffer/vector_buffer.cc
@@ -0,0 +1,64 @@
+#include "buffer/vector_buffer.h"
+
+#include <utility>
+#include <vector>
+
+namespace flux {
+
+VectorBuffer::VectorBuffer(std::vector<std::string> buffer)
+    : buffer_(std::move(buffer)) {}
+
+size_t VectorBuffer::Lines() const { return buffer_.size(); }
+
+size_t VectorBuffer::Size() const {
+  size_t size = 0;
+  for (const std::string& line : buffer_) {
+    size += line.size();
+  }
+
+  return size;
+}
+
+std::string VectorBuffer::GetLine(size_t row) const { return buffer_[row]; }
+
+Buffer::Position VectorBuffer::Insert(Buffer::Position pos, char c) {
+  std::string& line = buffer_[pos.row];
+  if (pos.col == line.length()) {
+    line.push_back(c);
+  } else {
+    line.insert(buffer_[pos.row].begin() + pos.col, c);
+  }
+
+  pos.col++;
+
+  return pos;
+}
+
+Buffer::Position VectorBuffer::BreakLine(Buffer::Position pos) {
+  std::string new_line = buffer_[pos.row].substr(pos.col);
+  buffer_[pos.row++].erase(pos.col);
+  buffer_.insert(buffer_.begin() + pos.row, std::move(new_line));
+  pos.col = 0;
+
+  return pos;
+}
+
+Buffer::Position VectorBuffer::Delete(Buffer::Position pos) {
+  if (pos.row == 0 && pos.col == 0) {
+    return pos;
+  }
+
+  if (pos.col == 0) {
+    buffer_.pop_back();
+    pos.row--;
+    pos.col = buffer_.back().length();
+
+    return pos;
+  }
+
+  buffer_[pos.row].erase(pos.col--, 1);
+
+  return pos;
+}
+
+}  // namespace flux
---
+++ buffer/vector_buffer.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+#include <vector>
+
+#include "buffer/buffer.h"
+
+namespace flux {
+
+class VectorBuffer : public Buffer {
+ public:
+  explicit VectorBuffer(std::vector<std::string> buffer);
+
+  ~VectorBuffer() = default;
+
+  virtual size_t Lines() const override;
+
+  virtual size_t Size() const override;
+
+  virtual std::string GetLine(size_t row) const override;
+
+  virtual Position Insert(Position pos, char c) override;
+
+  virtual Position BreakLine(Position pos) override;
+
+  virtual Position Delete(Position pos) override;
+
+ private:
+  std::vector<std::string> buffer_;
+};
+
+}  // namespace flux
