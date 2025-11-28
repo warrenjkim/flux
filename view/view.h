2d4897aea98067d34469a58f79d80e9d332fe4bc
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2025-11-28 10:42:56 -0800

    view API

---
+++ view/view.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "buffer/buffer.h"
+#include "terminal/raw_terminal.h"
+#include "view/cursor.h"
+#include "view/viewport.h"
+
+namespace flux {
+
+class View {
+ public:
+  explicit View(Buffer* buffer);
+
+  void Draw(RawTerminal* terminal);
+
+  void Resize(ViewPort viewport);
+
+  Cursor GetCursor() const;
+
+  Buffer::Position GetBufferPosition() const;
+
+  void UpdateCursor(Buffer::Position pos);
+
+  void MoveCursorUp();
+
+  void MoveCursorDown();
+
+  void MoveCursorLeft();
+
+  void MoveCursorRight();
+
+ private:
+  Buffer& buffer_;
+  ViewPort viewport_;
+  Cursor cursor_;
+};
+
+}  // namespace flux
