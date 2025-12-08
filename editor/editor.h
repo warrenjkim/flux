45ff0a132572ba114fa834228bd9d7b6e3810cb9
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2025-12-07 19:48:15 -0800

    Vim bindings (#7)

--- editor/editor.h
+++ editor/editor.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <unordered_map>
 
 #include "buffer/buffer.h"
 #include "editor/key_handler.h"
@@ -10,6 +11,8 @@
 
 namespace flux {
 
+enum class Mode { kNormal, kInsert };
+
 class Editor {
  public:
   explicit Editor();
@@ -18,6 +21,10 @@ class Editor {
 
  private:
   void BindKeys();
+
+  void BindNormalModeKeys();
+
+  void BindInsertModeKeys();
 
   std::unique_ptr<Buffer> OpenFile(std::string_view path) const;
 
@@ -30,7 +37,8 @@ class Editor {
   std::unique_ptr<StatusLine> status_line_;
   std::unique_ptr<CommandLine> command_line_;
   std::unique_ptr<View> view_;
-  KeyHandler key_handler_;
+  std::unordered_map<Mode, KeyHandler> key_handler_;
+  Mode mode_;
   bool running_;
 };
 
