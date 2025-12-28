23e9fe2640f9d58fe1536fc81bb18fabbf3d1d9a
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2025-12-27 23:29:25 -0800

    print buffer pos

--- view/status_line.cc
+++ view/status_line.cc
@@ -19,7 +19,17 @@ void StatusLine::Draw(RawTerminal* terminal, Options opts) {
       opts.file_name.substr(0, std::min(cols_, opts.file_name.length())));
 
   size_t i = opts.left_padding + opts.file_name.length();
-  while (++i < cols_) {
+  std::string position = to_string(opts.buffer_position);
+  size_t fill = cols_ < opts.right_padding + position.size()
+                    ? 0
+                    : cols_ - (opts.right_padding + position.size());
+  while (++i < fill) {
+    terminal->Write(opts.delimiter);
+  }
+
+  terminal->Write(position);
+
+  for (size_t i = 0; i < opts.right_padding; i++) {
     terminal->Write(opts.delimiter);
   }
 }
